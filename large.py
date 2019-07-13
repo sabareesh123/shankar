35be6f0188d7c911dbc4a851c392bb0d5d6968bc
shankar <52308621+shankar15nov98@users.noreply.github.com>
2019-07-13 16:45:47 +0530

    Update large.py

--- large.py
+++ large.py
@@ -1,7 +1,8 @@
-num1,num2,num3=int(input())
+num1,num2,num3=map(int,input().split())
 if num1>num2 and num1>num3:
-  print(num1)
+    
+    print(num1)
 elif num2>num1 and num2>num3:
-  print(num2)
+    print(num2)
 else:
-  print(num3)
+    print(num3)
