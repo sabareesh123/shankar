c8b888a3626243f6e5a44e9ecac6013601d8a0ab
shankar <52308621+shankar15nov98@users.noreply.github.com>
2019-07-16 11:27:19 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,17 @@
+
+
+num = 407
+
+if num > 1:
+
+   for i in range(2,num):
+       if (num % i) == 0:
+           print("no")
+           
+           break
+   else:
+       print("yes")
+       
+
+else:
+   print("no")
