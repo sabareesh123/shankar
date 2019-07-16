43c2f943e206476f4afbd9b450847f68e720455b
shankar <52308621+shankar15nov98@users.noreply.github.com>
2019-07-16 12:00:55 +0530

    Create arm int.py

---
+++ arm int.py
@@ -0,0 +1,22 @@
+
+
+m,n=map(int,input().split())
+
+
+for num in range(m,n+1):
+
+   
+   order = len(str(num))
+    
+   
+   sum = 0
+
+  
+   temp = num
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if num == sum:
+       print(num)
